e29c917f9bae4fae3a909557a56788790e524285
felicia <felicia.buren@ga.ntig.se>
2023-09-12 14:33:27 +0200

    first commit

---
+++ Bash-a-monster.cs
@@ -0,0 +1,6 @@
+﻿namespace Bash_a_monster;
+
+public class Bash_a_monster
+{
+
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Bash_a_monster;
+
+namespace Bash_a_monster;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
