82456eaee5946141e5007a96dba11f2eb0f9b3a8
felicia <felicia.buren@ga.ntig.se>
2023-09-18 11:05:53 +0200

    Börjat på raylib spelet bash a monster, börjat skapa spelarna

--- Program.cs
+++ Program.cs
@@ -1,11 +1,41 @@
 ﻿using Bash_a_monster;
+using Raylib_cs;
 
 namespace Bash_a_monster;
 
 class Program
 {
+    static bool hit = false;
+    static int health = 100;
+
+    static Rectangle player1Rectangle;
+    static Rectangle player2Rectangle;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Raylib.InitWindow(600, 600, "Bash-a-monster");
+        Raylib.SetTargetFPS(60);
+
+        // skapa två spelare 
+        player1Rectangle = new Rectangle(100, 500, 30, 50);
+
+        player2Rectangle = new Rectangle(500, 500, 30, 50);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Start();
+        }
+    }
+
+    // Rita ut spelet
+    static void Start()
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.BLUE);
+
+        // Rita ut spelarna 
+        Raylib.DrawRectangle((int)player1Rectangle.x, (int)player1Rectangle.y, 30, 50, Color.WHITE);
+
+        Raylib.EndDrawing();
     }
 }
